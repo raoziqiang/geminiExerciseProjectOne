5a6148fd81f684d1b08aa622ae294144f55feddd
raoziqiang <raoziqiang1995@163.com>
2026-07-30 18:57:08 -0700

    day 1

---
+++ study.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# author:raoziqiang
+# time:2026-7-30
+# description: This script is written by raoziqiang on 2026-7-30
+# usage: python3 script.py
+# notes: Add any additional notes or comments about the script here
+# aim study using python programming develop skills and agent development
+#print("Hello, world!")
+#print('are you ok?')
+#print('i\'m your "father"')
+#print(r"we' 'are' 'the' 'champions'")
+#print(r'''hello,\n
+#world''')
+#print(
+#'''line1
+#    line2
+#        line3     ''')
+
+5 > 3 and 3 > 1
+print(5 > 3 and 3 > 1)
+# int age ; 动态变量不需要先定义类型
+age = input ("Please enter your age: ")
+age = int(age)
+if age >= 18&age < 100:
+    print('adult')
+else:
+    print('teenager')
+
+n = 123
+f = 456.789
+s1 = "hello, world"
+s2 = 'hello, \'adam\''
+s3 = r'hello, "world"'
+s4 = '''hello,
+Bob!'''
+# print(?)
