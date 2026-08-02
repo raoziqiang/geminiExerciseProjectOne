6befa752098f77a72ed87343ee13fff8c551fbdb
raoziqiang <raoziqiang1995@163.com>
2026-08-01 20:10:07 -0700

    day2

--- study.py
+++ study.py
@@ -35,3 +35,23 @@ s3 = r'hello, "world"'
 s4 = '''hello,
 Bob!'''
 # print(?)
+
+
+# List:有序,可变的集合
+fruit = ['apple', 'banana', 'cherry','orange']
+print("Original List:", fruit) #{'apple', 'banana', 'cherry', 'orange'}
+# 索引访问(从0开始,负数表示从末尾开始计数)
+print("last element:", fruit[-1]) #orange
+print("first element:", fruit[0]) #apple
+
+# 切片[起始:结束:步长]，不包含结束位置-左闭右开
+print("slice:", fruit[1:3]) #['banana', 'cherry']
+print("slice with step:", fruit[::2]) #['apple', 'cherry']
+print("slice with negative step:", fruit[::-2]) #['orange',  'banana']
+print("slice last two:", fruit[-2:]) #['cherry', 'orange']
+
+#增删改查
+fruit.append('pear') #增加元素-末尾添加
+fruit.insert(1, 'grape') #增加元素-指定位置添加
+fruit.remove('banana') #删除元素-按值删除
+#待我抽支烟,有点困
